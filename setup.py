4123c2580a2e9b7f14d9284ae0a8986650954380
Tolo Palmer <toloco@gmail.com>
2015-03-25 21:45:54 +0000

    Fix p2 and p3 setup

--- setup.py
+++ setup.py
@@ -1,13 +1,6 @@
-"""A setuptools based setup module.
-
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
-"""
-
-# Always prefer setuptools over distutils
+import sys
+import os
 from setuptools import setup, find_packages
-# To use a consistent encoding
 from codecs import open
 from os import path
 
@@ -17,6 +10,26 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
+PY3 = sys.version_info[0] == 3
+
+# -*- Installation Requires -*-
+def strip_comments(l):
+    return l.split('#', 1)[0].strip()
+
+def reqs(*f):
+    return [
+        r for r in (
+            strip_comments(l) for l in open(
+                os.path.join(os.getcwd(), 'requirements', *f)).readlines()
+        ) if r]
+
+install_requires = reqs('base.txt')
+if not PY3:
+    install_requires += reqs('p26.txt')
+
+# -*- Tests Requires -*-
+tests_require = reqs('test.txt')
+
 setup(
     name='PyOanda',
 
@@ -24,18 +37,11 @@ setup(
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
     version='0.1.0',
-
     description='PyOanda',
     long_description=long_description,
-
-    # The project's main homepage.
     url='https://github.com/toloco/pyoanda',
-
-    # Author details
     author='Tolo Palmer',
     author_email='tolopalmer@gmail.com',
-
-    # Choose your license
     license='MIT',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
@@ -65,7 +71,7 @@ setup(
     ],
 
     # What does your project relate to?
-    keywords='oanda python wrap',
+    keywords='oanda, wrapper',
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
@@ -75,14 +81,17 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['requests'],
+    install_requires=install_requires,
+    tests_require=tests_require,
+    test_suite='nose.collector',
+
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
     # $ pip install -e .[dev,test]
-    extras_require={
-        'dev': ['check-manifest', 'ipdb'],
-        'test': ['coverage', 'pytest-sugar', 'nose'],
-    }
+    # extras_require={
+    #     'dev': ['check-manifest', 'ipdb'],
+    #     'test': ['coverage', 'pytest-sugar', 'nose'],
+    # }
 )
