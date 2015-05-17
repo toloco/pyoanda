6af18039d9cc400703de5b846257db838cd40f08
Tolo Palmer <toloco@gmail.com>
2015-05-17 16:24:22 +0100

    Fix the streamming urls

--- pyoanda/__init__.py
+++ pyoanda/__init__.py
@@ -1,6 +1,15 @@
 from .client import Client
 
 # OANDA API URLS
-SANDBOX = ("http://api-sandbox.oanda.com", "http://stream-sandbox.oanda.com/")
-PRACTICE = ("https://api-fxpractice.oanda.com", "https://stream-fxpractice.oanda.com/")
-TRADE = ("https://api-fxtrade.oanda.com", "https://stream-fxtrade.oanda.com/")
+SANDBOX = (
+    "http://api-sandbox.oanda.com",
+    "http://stream-sandbox.oanda.com"
+)
+PRACTICE = (
+    "https://api-fxpractice.oanda.com",
+    "https://stream-fxpractice.oanda.com"
+)
+TRADE = (
+    "https://api-fxtrade.oanda.com",
+    "https://stream-fxtrade.oanda.com"
+)
