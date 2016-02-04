48e9f5e9b856d61058d87e6403e4e2b0bb9652f3
Tolo Palmer <toloco@gmail.com>
2016-02-04 14:29:17 +0000

    PEP8 examples

--- examples/create_stop_order.py
+++ examples/create_stop_order.py
@@ -1,28 +1,24 @@
-import yaml,json,datetime
-from dateutil.parser import parse
-from pyoanda import Client, PRACTICE, TRADE
-from pyoanda.exceptions import BadRequest
-from datetime import *
+"""How to create a stop order without pulling out all your hair :-)
+"""
+import datetime
 
-# how to create a stop order without pulling out all your hair :-)
+from pyoanda import Client, PRACTICE, Order
 
-client = Client(environment=PRACTICE, account_id='replace with your account id', access_token='replace with your access token )
+client = Client(
+    environment=PRACTICE,
+    account_id="{{ACCOUNT_ID}}",
+    access_token="{{ACCOUNT_TOKEN}}"
+)
 
-class AbstractDict(object):__dict__ = {}
-#   ---- we use this to avoid error :: -----#
-# AttributeError: 'dict' object has no attribute '__dict__'
-# because 'create_order()' has this ::params=order.__dict__
-# ---------------------------------------------------------#
+test_order = Order(
+    instrument="EUR_JPY",
+    units=10,
+    side="buy",
+    type="stop",
+    stopLoss=80.95,
+    takeProfit=170.56,
+    price=10.0,
+    expiry=datetime.datetime.now() + datetime.timedelta(days=1)
+)
 
-timer = datetime.today() + timedelta(days=1)
-timer = timer.isoformat("T") + "Z" #expiration time in UTC.
-
-trade_params = {'instrument':'EUR_JPY','units':10,'side':'buy','type':'stop',
-                'stopLoss':80.95,'takeProfit':170.56,'price':83.34,'expiry':timer
-               }
-test = AbstractDict()
-test.__dict__= trade_params
-stop_order = client.create_order(order=test) 
-
-
-
+stop_order = client.create_order(order=test_order)
--- examples/multi_currency_prices.py
+++ examples/multi_currency_prices.py
@@ -1,41 +1,55 @@
 
-    from pyoanda import Client, PRACTICE
+from pyoanda import Client, PRACTICE
 
-    client = Client(environment=PRACTICE,account_id="Your Oanda account ID",access_token="Your Oanda access token")
+client = Client(
+    environment=PRACTICE,
+    account_id="{{ACCOUNT_ID}}",
+    access_token="{{ACCOUNT_TOKEN}}"
+)
 
-    # Get prices for a list of instruments
+# Get prices for a list of instruments
 
-    pair_list = ['AUD_JPY','EUR_JPY','GBP_JPY','AUD_USD']
+pair_list = ('AUD_JPY', 'EUR_JPY', 'GBP_JPY', 'AUD_USD')
 
-    dataset = client.get_prices(instruments=','.join(pair_list),stream=False)
+dataset = client.get_prices(
+    instruments=','.join(pair_list),
+    stream=False
+)
 
-     #json response::
-     {u'prices': [{u'ask': 81.551,
-         u'bid': 81.53,
-         u'instrument': u'AUD_JPY',
-         u'time': u'2016-01-26T07:39:56.525788Z'},
-        {u'ask': 127.975,
-         u'bid': 127.957,
-         u'instrument': u'EUR_JPY',
-         u'time': u'2016-01-26T07:39:55.712253Z'},
-        {u'ask': 167.269,
-         u'bid': 167.239,
-         u'instrument': u'GBP_JPY',
-         u'time': u'2016-01-26T07:39:58.333404Z'},
-        {u'ask': 0.69277,
-        u'bid': 0.6926,
-        u'instrument': u'AUD_USD',
-        u'time': u'2016-01-26T07:39:50.358020Z'}]}
-    
-    
-     # simplistic way of extracting data from the json response::
-    
-     aud_jpy = [d for d in dataset['prices'] if d['instrument']=='AUD_JPY']
-     bid = [d['bid'] for d in aud_jpy][-1]
-     ask = [d['ask'] for d in aud_jpy][-1]
-     time = [d['time'] for d in aud_jpy][-1]
-     
-     
+"""
+Response sample
+{
+    "prices": [
+        {
+            "ask": 81.551,
+            "bid": 81.53,
+            "instrument": "AUD_JPY",
+            "time": "2016-01-26T07:39:56.525788Z"
+        },
+        {
+            "ask": 127.975,
+            "bid": 127.957,
+            "instrument": "EUR_JPY",
+            "time": "2016-01-26T07:39:55.712253Z"
+        },
+        {
+            "ask": 167.269,
+            "bid": 167.239,
+            "instrument": "GBP_JPY",
+            "time": "2016-01-26T07:39:58.333404Z"
+        },
+        {
+            "ask": 0.69277,
+            "bid": 0.6926,
+            "instrument": "AUD_USD",
+            "time": "2016-01-26T07:39:50.358020Z"
+        }
+    ]
+}
+"""
 
-
-
+# simplistic way of extracting data from the json response::
+aud_jpy = [d for d in dataset['prices'] if d['instrument'] == 'AUD_JPY']
+bid = [d['bid'] for d in aud_jpy][-1]
+ask = [d['ask'] for d in aud_jpy][-1]
+time = [d['time'] for d in aud_jpy][-1]
